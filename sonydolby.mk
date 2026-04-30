beb64a98dbc14da92dd425fb67743001f117b4a1
Jammy555 <prathap.venkata@gmail.com>
2026-04-30 21:51:50 +0530

    Remove duplicated priv-app permission declaration

--- sonydolby.mk
+++ sonydolby.mk
@@ -20,10 +20,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     SonyDolbyResCommon
 
-# Permissions
-PRODUCT_COPY_FILES += \
-    $(DOLBY_PATH)/configs/permissions/privapp-permissions-dolby.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-dolby.xml
-
 # Sepolicy
 BOARD_VENDOR_SEPOLICY_DIRS += $(DOLBY_PATH)/sepolicy/vendor
 
